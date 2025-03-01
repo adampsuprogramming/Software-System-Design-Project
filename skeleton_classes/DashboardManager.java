52e4ae664b110351933c5788a8d9ee2173d4b397
adampsuprogramming <156355487+adampsuprogramming@users.noreply.github.com>
2025-03-01 14:17:43 -0500

    Update DashboardManager.java

--- skeleton_classes/DashboardManager.java
+++ skeleton_classes/DashboardManager.java
@@ -8,34 +8,37 @@ public class DashboardManager {
     private Map<String, Integer> userMap;
     private List<Integer> userIds;
     private Loan loan;
+    private static DashboardManager obj; // Store Singleton object
+
+    private DashboardManager() {} // Private constructor for Singleton pattern
 
     public String updateDashboard(List<String> selections) {
         this.selections=selections;
-        // Code here calling createDBRequest helper class
-        // Code here getting Singleton instance of DatabaseAccess Class
-        // Code here calling DatabaseAccess class with database request via reqDasahboardData method
-        // Code here calling parseData class
-        // Code here getting Singleton instance of LoanFactory class
-        // Code here calling LoanFactory class and passing ParsedData to get loan and setting loan instance variable equal to it via createLoan method
-        // Code here getting Singleton instance of ReportSysGateway class
-        // Code here calling passing parsed data to ReportSysGateway via generateDashboard method and getting view in return
+        // 1. Call createDBRequest helper class
+        // 2. Get Singleton instance of DatabaseAccess Class
+        // 3. Call DatabaseAccess class with database request via reqDasahboardData method
+        // 4. Call parseData class
+        // 5. Get Singleton instance of LoanFactory class
+        // 6. Call LoanFactory class and pass ParsedData to get loan and setting loan instance variable equal to it via createLoan method
+        // 7. Get Singleton instance of ReportSysGateway class
+        // 8. Pass parsed data to ReportSysGateway via generateDashboard method and get view in return
         return null; // Returns formatted view of the Dashboard
     }
 
     private String createDBRequest(List<String> selections) {
-        //Code here creating formatted request
+        //Create formatted request
         return null; // returns formatted Database Request
     }
 
     private String parse(String dashboardData) {
-        // Code here parsing dashBoardData
+        // Parse dashBoardData
         return null; // returns Parsed Dashboard Data
     }
 
     public Map<String, Integer> lookupUser(String name) {
-        // Code here getting Singleton instance of DatabaseAccess Class
-        // Code here calling DatabaseAccess class with username request via reqUserID method
-        // Code here calling createMap helper method passing data returned from DB and setting userMap instance variable equal to it
+        // 1. Get Singleton instance of DatabaseAccess Class
+        // 2. Call DatabaseAccess class with username request via reqUserID method
+        // 3. Call createMap helper method passing data returned from DB and setting userMap instance variable equal to it
         return null; // returns map of usernames to user ids
     }
 
@@ -49,13 +52,13 @@ public class DashboardManager {
     }
 
     public String saveCommentary(String commentary) {
-        // Code here creating Commentary object
-        // Code here calling processCommentary to format it appropriately
-        // Code here adding commentary to Loan object
-        // Code here getting Singleton instance of DatabaseAccess Class
-        // Code here calling on DatabaseAccess class and passing loan object via updateLoan method
-        // Code here getting Singleton instance of EmailSystemGateway Class
-        // Code here calling on emailCommentary method in EmailSystemGateway Class to send commentary via email
+        // 1. Creating Commentary object
+        // 2. Call processCommentary to format it appropriately
+        // 3. Add commentary to Loan object
+        // 4. Get Singleton instance of DatabaseAccess Class
+        // 5. Call on DatabaseAccess class and passing loan object via updateLoan method
+        // 6. Get Singleton instance of EmailSystemGateway Class
+        // 7. Call on emailCommentary method in EmailSystemGateway Class to send commentary via email
         return null; // Returns confirmation
     }
 
@@ -64,5 +67,9 @@ public class DashboardManager {
         return null; // return processed commentary
     }
 
+    public static DashboardManager getInstance() {
+        // Singleton code here to check if obj exists.  If it does, return it, otherwise create it and then return it.
+        return obj; // returns object
+    }
 
 }
